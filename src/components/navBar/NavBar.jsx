97ae250ac7cf24408eb6a4dad5b29e07bea5ad9c
Pasindusheshan127 <pasinduhettiarachchi201@gmail.com>
2024-08-02 10:01:59 +0530

    create components

---
+++ src/components/navBar/NavBar.jsx
@@ -0,0 +1,14 @@
+import Link from "next/link";
+import Links from "./Links/Links";
+
+const NavBar = () => {
+  return (
+    <div>
+      <div>logo</div>
+      <div>
+        <Links />
+      </div>
+    </div>
+  );
+};
+export default NavBar;
